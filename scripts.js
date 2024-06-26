929e9f7c90a4fde444216e2fed3020ed9b1ff013
Werqzy <131708513+Werqzy@users.noreply.github.com>
2024-06-26 11:26:28 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref, push, onValue } from "firebase/database";
+import { getDatabase, ref, child, push, onValue } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
