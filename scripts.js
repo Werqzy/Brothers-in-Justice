98543f9640de7e05e88af94b239eda66fe50dcfa
Werqzy <131708513+Werqzy@users.noreply.github.com>
2024-06-25 22:59:58 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function switchLanguage() {
+    const elements = document.querySelectorAll('[data-en], [data-fr]');
+    elements.forEach(element => {
+        const enText = element.getAttribute('data-en');
+        const frText = element.getAttribute('data-fr');
+        if (document.documentElement.lang === 'fr') {
+            element.innerText = enText;
+        } else {
+            element.innerText = frText;
+        }
+    });
+    document.documentElement.lang = document.documentElement.lang === 'fr' ? 'en' : 'fr';
+}
